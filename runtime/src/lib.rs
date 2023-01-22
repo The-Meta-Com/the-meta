1a8882c34860eebf70d82fa520fab68642da07a2
Substrate Developer <nvsamardak@gmail.com>
2023-01-22 21:57:58 +0000

    add sudo pallet

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -453,6 +453,11 @@ impl pallet_template::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 }
 
+impl pallet_sudo::Config for Runtime {
+	type RuntimeEvent = RuntimeEvent;
+	type RuntimeCall = RuntimeCall;
+}
+
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
 	pub enum Runtime where
@@ -487,6 +492,7 @@ construct_runtime!(
 
 		// Template
 		TemplatePallet: pallet_template::{Pallet, Call, Storage, Event<T>}  = 40,
+		Sudo : pallet_sudo::{Pallet, Call, Storage, Event<T>, Config<T>}  = 41,
 	}
 );
 
